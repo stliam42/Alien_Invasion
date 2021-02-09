a9f23b036a917321662a626036aff83a52c96a00
Alex <alexk@DESKTOP-HD74KLR>
2021-02-09 17:26:40 +1000

    Fullscreen

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -16,6 +16,10 @@ class AlienInvasion():
         self.bg_color = (230, 230, 230)
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_height = self.screen.get_rect().height
+        #self.settings.screen_width = self.screen.get_rect().width
         pygame.display.set_caption("Alien Invasion!")
 
         self.ship = Ship(self)
@@ -49,6 +53,9 @@ class AlienInvasion():
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        # Exit by 'Q'
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
