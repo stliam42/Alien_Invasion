d939f6aa5a9e6543bbabc225e899e32f2ad617e6
Alex <alexk@DESKTOP-HD74KLR>
2021-02-09 13:47:23 +1000

    Create a ship class

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -1,33 +1,44 @@
 import sys
 import pygame
 
-for settings import Settings
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion():
-    """ Class for resourses managment and game behavior"""
+    """ Class for resourses managment and game behavior. """
 
     def __init__(self):
-        """ Initialize the game and create game's resourses"""
+        """ Initialize the game and create game's resourses. """
         pygame.init()
         self.settings = Settings()
 
         # Color
         self.bg_color = (230, 230, 230)
 
-        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion!")
 
+        self.ship = Ship(self)
+
     def run_game(self):
-        """ Run the game cycle"""
+        """ Run the game cycle """
         while True:
-            # Tracking the keyboard and mouse
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            
+
+    def _check_events(self):
+        """ Tracking the keyboard and mouse """
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            # Change screen
-            self.screen.fill(self.settings.bg_color)
-            # Display the last screen
-            pygame.display.flip()
+
+    def _update_screen(self):
+        """ Change and display screen """
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Create the instance and start the game
---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship():
+    """ Class for ship managment"""
+
+    def __init__(self, ai_game):
+        """ Initialize the ship and start position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Download a ship image and get rectangle
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Every new ship spawn at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """ Paint the ship in the current position"""
+        self.screen.blit(self.image, self.rect)
